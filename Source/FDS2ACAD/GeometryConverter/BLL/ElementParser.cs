e2fcb7912aa1ca5d870c4957b6aed4c6f82daf10
Alexander Konovkov <501648+NR4TR@users.noreply.github.com>
2010-02-28 20:09:00 +0000

    emplementation of fds file generation started

---
+++ Source/FDS2ACAD/GeometryConverter/BLL/ElementParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using GeometryConverter.DAL.Collections;
+
+namespace GeometryConverter.BLL
+{
+    public static class ElementParser
+    {
+        public static string ToString(this ElementCollection collection)
+        {
+            StringBuilder output = new StringBuilder();
+
+
+
+            return output.ToString();
+        }
+    }
+}
